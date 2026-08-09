10c898d2bd38eae1ffc281165afa55193254beba
aadennis <dennis.circ@gmail.com>
2026-08-09 15:46:51 +0100

    name output file

--- get_split_values.py
+++ get_split_values.py
@@ -1,5 +1,5 @@
 import pandas as pd
-
+from pathlib import Path
 
 CSV_FILE = "data/split01.csv"
 df = pd.read_csv(CSV_FILE)
@@ -7,7 +7,14 @@ print(df.head())
 
 mf = df[["Laps","Time","Cumulative Time","Distance km","Avg Pace min/km","Avg HR bpm",
          "Max HR bpm","Avg Run Cadence spm","Avg Ground Contact Time ms",
-         "Avg Stride Length m","Avg Vertical Oscillation cm","Avg Vertical Ratio %"]]
+         "Avg Stride Length m","Avg Vertical Oscillation cm","Avg Vertical Ratio %",
+         "Best Pace min/km","Max Run Cadence spm"
+         ]]
 print(mf.head())
 
-mf.to_csv("c:/temp/aaa.csv", index=False)
+p = Path(CSV_FILE)
+output_file = f"c:/temp/{p.stem}.out.csv"
+
+print(f"output file is here: [{output_file}]")
+
+mf.to_csv(output_file, index=False)
