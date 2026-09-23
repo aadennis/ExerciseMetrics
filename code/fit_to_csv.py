30665e0b044b95bf70aa1aec9c164f6bad5cd66a
Dennis <dennis.circ@gmail.com>
2026-09-23 15:52:45 +0100

    er

--- code/fit_to_csv.py
+++ code/fit_to_csv.py
@@ -139,7 +139,7 @@ def build_path(file, is_output: bool) -> str:
 
 if __name__ == "__main__":
     """Run the conversion using the default example data file."""
-    input_file = "2026-05-25-intervals.fit"
+    input_file = "2026-09-23-flat.fit"
     output_file = f"{input_file.rsplit('.', 1)[0]}-fit.csv"
     input_file = build_path(input_file, False)
     output_file = build_path(output_file, True)
